138323091d8806026a0682417876be76c81cf1cd
saswatsamal <samalsaswat0@gmail.com>
2021-04-19 20:16:09 +0530

    Converting the image into grayscale image

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 #Reading the face image
 img = cv2.imread('testFace.jpg')
+
+#Converting the image into grayscale image
+gray = cv2.cvtColor(img, cv2.COLOR_BAYER_BG2BGR)
+
