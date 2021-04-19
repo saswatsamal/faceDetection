9154229233424928b6619c516d1c57a0d45dee4a
saswatsamal <samalsaswat0@gmail.com>
2021-04-19 20:15:10 +0530

    Reading the face image

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ import cv2
 
 #Loading the haarcascade front face file
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#Reading the face image
+img = cv2.imread('testFace.jpg')
