2e6e73405023728e2972f267c0838ef319e4c7cb
saswatsamal <samalsaswat0@gmail.com>
2021-04-19 20:50:02 +0530

    User based system

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 #run.py
 import os
-os.system('python main.py')
+print("Welcome to the Face Detection Algorithm/nUsing this program you can see a face in a photo getting recognized by OpenCV and HaarCascade")
+print("If you want to execute the program type Y else N")
+userInput = str(input())
+
+if (userInput=='Y' or userInput=='y'):
+    os.system('python main.py')
+else:
+    print('Thank you for using')
